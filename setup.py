bf404a865532ce008efdd93313005fe82a9a8a3d
Sam Woolerton <samw@fosters.co.nz>
2020-09-08 17:04:41 +1200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="tap-acuite",
+    version="1.0.0",
+    description="Singer.io tap for extracting data from the Acuite API",
+    author="Sam Woolerton",
+    url="https://samwoolerton.com",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_acuite"],
+    install_requires=["singer-python==5.3.3", "requests==2.20.0"],
+    extras_require={"dev": ["pylint", "ipdb", "nose",]},
+    entry_points="""
+          [console_scripts]
+          tap-acuite=tap_acuite:main
+      """,
+    packages=["tap_acuite"],
+    package_data={"tap_acuite": ["tap_acuite/schemas/*.json"]},
+    include_package_data=True,
+)
