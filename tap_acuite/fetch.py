eeb1f3b65fe286435dc00f86053789af84b9b58c
Sam Woolerton <samw@fosters.co.nz>
2020-09-09 15:36:41 +1200

    Refactor shared Stitch metadata operations into functions

--- tap_acuite/fetch.py
+++ tap_acuite/fetch.py
@@ -1,7 +1,6 @@
 import singer
 import singer.metrics as metrics
 from singer import metadata
-from singer.bookmarks import get_bookmark
 from tap_acuite.utility import get_generic, get_all_pages, formatDate
 
 
@@ -12,83 +11,66 @@ def get_paginated(resource, url=""):
         url = resource
 
     def get(schema, state, mdata):
-        with metrics.record_counter(resource) as counter:
-            for page in get_all_pages(resource, url):
-                for row in page:
-                    with singer.Transformer() as transformer:
-                        rec = transformer.transform(
-                            row, schema, metadata=metadata.to_map(mdata)
-                        )
-                        singer.write_record(
-                            resource, rec, time_extracted=extraction_time
-                        )
-
-                    counter.increment()
-
-                    singer.write_bookmark(
-                        state, resource, "since", formatDate(extraction_time),
-                    )
-            return state
+        rows = [row for page in get_all_pages(resource, url) for row in page]
+        write(rows, resource, schema, mdata, extraction_time)
+        return write_bookmark(state, resource, extraction_time)
 
     return get
+
+
 def get_projects(schemas, state, mdata):
     extraction_time = singer.utils.now()
 
-    with metrics.record_counter("projects") as counter:
-        with metrics.record_counter("attendances") as a_counter:
-            for page in get_all_pages(
-                "projects", "projects", {"includeArchived": "true"}
-            ):
-                for row in page:
-                    # handle sites record
-                    with singer.Transformer() as transformer:
-                        rec = transformer.transform(
-                            row, schemas["projects"], metadata=metadata.to_map(mdata)
-                        )
-                        singer.write_record(
-                            "projects", rec, time_extracted=extraction_time
-                        )
+    rows = [
+        row
+        for page in get_all_pages("projects", "projects", {"includeArchived": "true"})
+        for row in page
+    ]
+    write(rows, "projects", schemas["projects"], mdata, extraction_time)
 
-                    counter.increment()
-
+    for project in rows:
                     if schemas.get("audits"):
                         get_detailed(
                             "audits",
-                            "projects/{}/audits".format(row["Id"]),
+                "projects/{}/audits".format(project["Id"]),
                             schemas,
                             state,
                             mdata,
                         )
 
                     if schemas.get("rfis"):
-                        get_paginated("rfis", "projects/{}/rfi".format(row["Id"]))
+            get_paginated("rfis", "projects/{}/rfi".format(project["Id"]))
 
-    singer.write_bookmark(
-        state, "projects", "since", formatDate(extraction_time),
-    )
-            return state
+
+
+    return write_bookmark(state, "projects", extraction_time)
 
 
 def get_detailed(resource, url, schemas, state, mdata):
     extraction_time = singer.utils.now()
 
-    r = get_generic(resource, url)
-    rows = r["Data"]
+    def get_detail(row):
+        r = get_generic(resource, "{}/{}".format(url, row["Id"]))
+        return r["Data"]
 
+    r = get_generic(resource, url)
+    details = [get_detail(row) for row in r["Data"]]
+
+    write(details, resource, schemas[resource], mdata, extraction_time)
+    return write_bookmark(state, resource, extraction_time)
+
+
+def write(rows, resource, schema, mdata, dt):
     with metrics.record_counter(resource) as counter:
         for row in rows:
-            res = get_generic(resource, "{}/{}".format(url, row["Id"]))
-            detail = res["Data"]
-
             with singer.Transformer() as transformer:
                 rec = transformer.transform(
-                    detail, schemas[resource], metadata=metadata.to_map(mdata)
+                    row, schema, metadata=metadata.to_map(mdata)
                 )
-                singer.write_record(resource, rec, time_extracted=extraction_time)
-
+                singer.write_record(resource, rec, time_extracted=dt)
             counter.increment()
 
-    singer.write_bookmark(
-        state, resource, "since", formatDate(extraction_time),
-    )
+
+def write_bookmark(state, resource, dt):
+    singer.write_bookmark(state, resource, "since", formatDate(dt))
     return state
